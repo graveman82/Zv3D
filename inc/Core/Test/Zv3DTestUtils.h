7b77a57aaa3de3c88554bccdd60c997770add615
mrsung82 <mrsung82@gmail.com>
2022-12-23 14:08:08 +0300

    Test implemented.

---
+++ inc/Core/Test/Zv3DTestUtils.h
@@ -0,0 +1,15 @@
+#ifndef ZV3D_CORE_TEST_TESTUTILS_H
+#define ZV3D_CORE_TEST_TESTUTILS_H
+
+// Zv3D Engine. (C) 2012-2022 Marat Sungatullin (aka graveman, aka mrsung)
+//
+// License: double license GPLv3 and zlib (see "Zv3DLicense.txt").
+// 
+// Purpose: test utils (decl).
+/*
+	23-Dec-2022	grv creation.
+*/
+
+void ZvdTestWaitKey();
+
+#endif // ZV3D_CORE_TEST_TESTUTILS_H
---
+++ src/Core/Test/Zv3DTestUtils.cpp
@@ -0,0 +1,19 @@
+// Zv3D Engine. (C) 2012-2022 Marat Sungatullin (aka graveman, aka mrsung)
+//
+// License: double license GPLv3 and zlib (see "Zv3DLicense.txt").
+// 
+// Purpose: test utils impl.
+/*
+	23-Dec-2022	grv creation.
+*/
+#include "stdafx.h"
+#include "Core/Common/Zv3DAssert.h"
+
+#include <iostream>
+
+void ZvdTestWaitKey()
+{
+	std::cout << "Press any key to continue..." << std::endl;
+	std::string qs;
+	std::cin >> qs;
+}
