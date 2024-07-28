5d68840df4eeed4c30b55f30f04f978e9a876db1
mrsung82 <mrsung82@gmail.com>
2024-07-28 08:28:58 +0300

    Common utils added

---
+++ inc/Core/Common/Zv3DCommonUtils.h
@@ -0,0 +1,17 @@
+#ifndef ZV3D_CORE_COMMON_COMMONUTILS_H
+#define ZV3D_CORE_COMMON_COMMONUTILS_H
+
+// Zv3D Engine. (C) 2012-2022 Marat Sungatullin (aka graveman, aka mrsung)
+//
+// License: double license GPLv3 and zlib (see "Zv3DLicense.txt").
+// 
+// Purpose: utils shared over the whole code.
+/*
+	19-Dec-2022	grv creation.
+*/
+
+#include "Core/Common/Zv3DByteUtils.h"
+
+
+#define ZVD_UNUSED(someVar) {(void)(someVar);}
+#endif // ZV3D_CORE_COMMON_COMMONUTILS_H
